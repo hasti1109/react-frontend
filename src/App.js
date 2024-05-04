e369d311e55aaceec0e6af936bb4878cd310f98e
Hasti Gabani <hastigabani1109@gmail.com>
2024-05-04 18:20:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState} from 'react';
+import './styles/App.css'
+import Card from './components/Card';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://my-json-server.typicode.com/Codeinwp/front-end-internship-api/posts');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((item, index) => (
+        <Card key={index} data={item} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react'
+import '../styles/Modal.css'
+import { X } from 'lucide-react'
+
+const Modal = ({data, onClose}) => {
+
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current ===  e.target){
+      onClose();
+    }
+  }
+
+  return (
+    <div ref={modalRef} onClick={closeModal} className='modal-container'>
+      <div className="modal-overlay">
+        <div className="modal-card">
+          <div className='quit-button'><X onClick={onClose} className='cross'/></div>
+          <img src={data.thumbnail.small} alt="Card" className="modal-image" />
+          <div className="modal-card-content">
+            <h2 className="modal-card-title">{data.title}</h2>
+            <p className="modal-card-description">{data.content}</p>
+            <div className="modal-card-footer">
+              <span className="modal-author">{data.author.name} - {data.author.role}</span>
+              <span className="modal-date"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
